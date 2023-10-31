4185d22e37b4e6ee5f913a3b34cec6f84232d9f2
jlsantia21 <jlsantia21@gmail.com>
2023-10-30 22:25:18 -0700

    Added files for Select Elements assignment

---
+++ README.md
@@ -0,0 +1,1 @@
+# Repository containing Select Elements assignment.
---
+++ script.js
@@ -0,0 +1,15 @@
+// select each element and log them to the console
+const h1 = document.querySelector("h1");
+const h3 = document.querySelector("h3");
+const ul = document.querySelector("ul");
+const li = document.querySelector("li");
+
+// select multiple elements with querySelectorAll()
+const lis = document.querySelectorAll("li");
+
+// log them to the console
+console.log(h1);
+console.log(h3);
+console.log(ul);
+console.log(li);
+console.log(lis);
